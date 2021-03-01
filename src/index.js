4f4c0ed5b0b369e7b89ef9c2664eb1d941420cfe
chinweokwu <paulcmorah@gmail.com>
2021-03-01 23:30:46 +0100

    Initialize Redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import rootReducer from './reducers/index';
 import reportWebVitals from './reportWebVitals';
+
+const reduxStore = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
